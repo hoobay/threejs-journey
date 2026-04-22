46ba1b7d4e0ca391bf8c2541112f6e18cff457cb
hoobay <hoobay69@gmail.com>
2026-04-22 12:27:34 +0200

    transform objects: learned about position, scale, rotation, axes helper, groups

--- transform-objects/src/script.js
+++ transform-objects/src/script.js
@@ -9,10 +9,59 @@ const scene = new THREE.Scene()
 /**
  * Objects
  */
-const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
-const mesh = new THREE.Mesh(geometry, material)
-scene.add(mesh)
+// const geometry = new THREE.BoxGeometry(1, 1, 1)
+// const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
+// const mesh = new THREE.Mesh(geometry, material)
+// scene.add(mesh)
+const group = new THREE.Group()
+group.position.y = 1
+group.scale.y = 2
+group.rotation.y = 1
+scene.add(group)
+
+const cube1 = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1),
+    new THREE.MeshBasicMaterial({ color: 0xff0000 })
+)
+group.add(cube1)
+
+const cube2 = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1),
+    new THREE.MeshBasicMaterial({ color: 0x00ff00 })
+)
+cube2.position.x = -2
+group.add(cube2)
+
+const cube3 = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1),
+    new THREE.MeshBasicMaterial({ color: 0x0000ff})
+)
+cube3.position.x = 2
+group.add(cube3)
+
+// Position
+// mesh.position.x = 0.7
+// mesh.position.y = -0.6
+// mesh.position.z = 1
+// mesh.position.set(0.7, -0.6, 1) // same as 3 above
+// mesh.position.normalize() -> reduces length of vector to 1 while preserving direction
+// console.log(mesh.position.length()) -> gets vector length
+// console.log(mesh.position.distanceTo(camera.position)) -> distance from another Vector3
+
+// Scale
+// mesh.scale.x = 2
+// mesh.scale.y = 0.5
+// mesh.scale.z = 0.5
+// mesh.scale.set(2, 0.5, 0.5) // same as 3 above
+
+// Axes Helper
+const axesHelper = new THREE.AxesHelper() // shows axes, move camera to see z axis
+scene.add(axesHelper)
+
+// Rotation
+// mesh.rotation.reorder('YXZ') // changes the order of axis rotation
+// mesh.rotation.x = Math.PI * 0.25
+// mesh.rotation.y = Math.PI * 0.25
 
 /**
  * Sizes
@@ -25,9 +74,11 @@ const sizes = {
 /**
  * Camera
  */
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height) 
 camera.position.z = 3
 scene.add(camera)
+
+// camera.lookAt(mesh.position) // makes the camera look at the cube
 
 /**
  * Renderer
